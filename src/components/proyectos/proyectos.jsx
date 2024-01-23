548954f69318d2c3e19d5eed8fe1412671bd6a8c
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-23 17:26:10 -0300

    commit nuevio

--- src/components/proyectos/proyectos.jsx
+++ src/components/proyectos/proyectos.jsx
@@ -16,13 +16,13 @@ const Proyectos = () => {
                 <h2>Aromas PLC</h2>
                 <p>Proyecto realizado para un emprendimiento de difusores y velas aromatizantes. Este proyecto está desarrollado en React Js para el Front-End y Node Js con Express Js y MongoDB para el Back-End.</p>
                 <div className={stylesProyectos.tecnologias}>
-                    <a href="https://react.dev" target='blank'><img src='/src/img/react-logo-48.png' alt="react js" title='React Js' /></a>
-                    <a href="https://nodejs.org/en" target='blank'><img src="/src/img/nodejs-logo-48.png" alt="node js" title='Node Js'/></a>
-                    <a href="https://www.mongodb.com/es" target='blank'><img src="/src/img/mongodb-logo-48.png" alt="mongodb" title='MongoDB'/></a>
+                    <a href="https://react.dev" target='blank'><img src='src/img/react-logo-48.png' alt="react js" title='React Js' /></a>
+                    <a href="https://nodejs.org/en" target='blank'><img src="src/img/nodejs-logo-48.png" alt="node js" title='Node Js'/></a>
+                    <a href="https://www.mongodb.com/es" target='blank'><img src="src/img/mongodb-logo-48.png" alt="mongodb" title='MongoDB'/></a>
                 </div>
                 <a href='https://aromasplc.vercel.app' target='blank'>
                     <div className={stylesProyectos.screen}>
-                        <div className={stylesProyectos.screenAromas}></div>
+                        <img src="src/img/aromasplcScreen.png" alt="" width='100%' height='100%' />
                     </div>
                 </a>
             </div>
@@ -30,12 +30,12 @@ const Proyectos = () => {
                 <h2>All New Style</h2>
                 <p>Proyecto realizado para un curso de React Js de CoderHouse. Este proyecto está desarrollado en React Js para el Front-End y Firebase para simular el Back-End</p>
                 <div className={stylesProyectos.tecnologias}>
-                    <a href="https://react.dev" target='blank'><img src='/src/img/react-logo-48.png' alt="react js" title='React Js' /></a>
-                    <a href="https://firebase.google.com/?hl=es" target='blank'><img src="/src/img/firebase-logo-48.png" alt="firebase" title='Firebase'/></a>
+                    <a href="https://react.dev" target='blank'><img src='src/img/react-logo-48.png' alt="react js" title='React Js' /></a>
+                    <a href="https://firebase.google.com/?hl=es" target='blank'><img src="src/img/firebase-logo-48.png" alt="firebase" title='Firebase'/></a>
                 </div>
                 <a href='https://allnewstyle-five.vercel.app/' target='blank'>
                     <div className={stylesProyectos.screen}>
-                        <img src="/src/img/allnewstyleScreen.png" alt="" width='100%' height='100%' />
+                        <img src="src/img/allnewstyleScreen.png" alt="" width='100%' height='100%' />
                     </div>
                 </a>
             </div>
