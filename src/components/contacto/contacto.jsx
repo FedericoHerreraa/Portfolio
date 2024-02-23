da4a8e045b91aee1a15b8855d37dc98d2c73e95a
federicoherrera <fede.juan.herrera@gmail.com>
2024-02-23 17:10:49 -0300

    commit

--- src/components/contacto/contacto.jsx
+++ src/components/contacto/contacto.jsx
@@ -10,19 +10,19 @@ const Contacto = () => {
                 <CodeIcon fontSize='large'/>
             </div>
             <div className={stylesContacto.info}>
-                <div>
+                <div style={{ marginBottom: '5%' }}>
                     <div className={stylesContacto.gmailLogo}></div>
                     <div style={{ marginLeft: '20px'}}>
                         <h2>fede.juan.herrera@gmail.com</h2>
                     </div>
                 </div>
-                <div>
+                <div style={{ marginBottom: '5%' }}>
                     <div className={stylesContacto.phoneLogo}></div>
                     <div style={{ marginLeft: '20px'}}>
                         <h2>+54 9 11 6296-4493</h2>
                     </div>
                 </div>
-                <div>
+                <div style={{ marginBottom: '5%' }}>
                     <a href="https://www.linkedin.com/in/federico-herrera-828636244/" target='blank'>
                         <div className={stylesContacto.linkedinLogo}></div>
                         <div style={{ marginLeft: '20px'}}>
